2a60b39e529e6809db978372165a74d0f151b567
vahan3317 <vahanvardanyan156@gmail.com>
2022-06-24 22:53:27 +0300

    initial

---
+++ footer.php
@@ -0,0 +1,6 @@
+<div id="footer">
+        <p>Test Register System <?php echo date("Y"); ?></p>
+    </div>
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,17 @@
+<?php 
+    include_once 'header.php'
+?>
+
+    <h1 id="index-text">Welcome, <?php if(isset($_SESSION['usersId'])){
+        echo explode(" ", $_SESSION['usersName'])[0]; 
+        
+        echo ":)";
+    }else{
+        echo 'Guest :)';
+    } 
+    ?> </h1>
+    
+
+<?php 
+    include_once 'footer.php'
+?>
